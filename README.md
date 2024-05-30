8b31fc44d3cfe569b100ef6132e9e2f438cf7353
Štěpán Koliáš <stepikkolik@Stepans-MacBook-Air.local>
2024-05-30 09:53:33 +0200

    deletion

--- README.md
+++
@@ -1,3 +0,0 @@
-# commerzbank_new
-# commerzbank_new
-# commerzbank_new_02
--- commerzbank_real_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CommerzbankRealAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'commerzbank_real_app'
